1941f2ab5d09c779bbde1728ae36ef75debabfbf
Yifei Zhang <yifei7.zhang@gmail.com>
2020-10-18 18:20:44 -0400

    deployment?

---
+++ precache-manifest.c1be0c23ab0756e2846180a12d593b8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cfab53a64c703a59bb096043881081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "438576beaadf0f6a1d16",
+    "url": "/static/css/main.1f5ea79a.chunk.css"
+  },
+  {
+    "revision": "7dbedd7e8a9c7491a220",
+    "url": "/static/js/2.414da3c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.414da3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438576beaadf0f6a1d16",
+    "url": "/static/js/main.2646396f.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "9c13cf96e041eb1823f2d867506a806d",
+    "url": "/static/media/background4.9c13cf96.jpg"
+  },
+  {
+    "revision": "dd4741965958371d09c55f7c32aef9ab",
+    "url": "/static/media/favicon.dd474196.jpg"
+  }
+]);
